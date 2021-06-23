57d32986873b0fd78392fc9a9dca1250bad6520a
Juhyun Lee <leeju@korea.ac.kr>
2021-06-23 15:04:23 +0900

    Update ver.0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PIC',
-    version='0.0.1',
+    version='0.0.2',
     description='Patent with Indirect Citation',
     url='https://github.com/lee-ju/PIC.git',
     author='Juhyun Lee',
